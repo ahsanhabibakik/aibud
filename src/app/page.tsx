3b5ed60537071b3a5c3dc706503cf73d9fade62c
delwerhossain <delwerhossain006@gmail.com>
2025-08-18 21:01:49 +0600

    fast commit: file-by-file: src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <UnifiedNavBar />
       <HeroSection />
 
-      <TargetAudienceSection />
+      {/* <TargetAudienceSection /> */}
 
       <FlagshipProductSection />
       <OurServicesSection />
